b602fa9a4ae4272dfed6fc0504211d4e408c9140
mathocihara <100290301+mathocihara@users.noreply.github.com>
2026-05-23 15:24:52 -0300

    construção:struct, funcoes, ponteiro. simbolicos

--- matriz.c
+++ matriz.c
@@ -3,3 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "matriz.h"
+
+/*struct matriz{
+    int numero;
+
+}*/
+/*no x_y {
+    struct matriz dado;
+    panteiro para o proximo nó na mesma linha (right)
+    panteiro para o proximo nó na mesma coluna (right)
+}*/
+//função para criar matriz
+//função vazia
+//funcao verificar existencia
+//funcao consultar nó
+//funcao insere final
+//função insere início
+//função tamanho matriz(x_y)
+//primeiraCelulaLinha()( ultima celula de cada lista tem que acessar a primeira celualr da litsa)
