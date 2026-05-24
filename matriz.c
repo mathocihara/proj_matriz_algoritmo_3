51ad26b39d52b6bc66d4e7c1ceb27a582ae2e1b0
mathocihara <100290301+mathocihara@users.noreply.github.com>
2026-05-24 11:28:45 -0300

    organização dos arquivos

--- matriz.c
+++ matriz.c
@@ -4,15 +4,7 @@
 #include <stdlib.h>
 #include "matriz.h"
 
-/*struct matriz{
-    int numero;
 
-}*/
-/*no x_y {
-    struct matriz dado;
-    panteiro para o proximo nó na mesma linha (right)
-    panteiro para o proximo nó na mesma coluna (right)
-}*/
 //função para criar matriz
 //função vazia
 //funcao verificar existencia
@@ -20,4 +12,4 @@
 //funcao insere final
 //função insere início
 //função tamanho matriz(x_y)
-//primeiraCelulaLinha()( ultima celula de cada lista tem que acessar a primeira celualr da litsa)
+//primeiraCelulaLinha()( ultima celula de cada lista tem que direcionar para a primeira celualr da litsa)
