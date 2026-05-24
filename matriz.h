f0bd731195afc09d9f105083efbad7e293b7bbc5
mathocihara <100290301+mathocihara@users.noreply.github.com>
2026-05-24 11:29:16 -0300

    struct matriz, struct elemento

--- matriz.h
+++ matriz.h
@@ -1,1 +1,27 @@
 // as funões de alocação, leitura, escrita e liberação de memória
+struct matriz{
+    struct elemento *inicio;
+    struct elemento *fim;
+
+}
+typedef struct matriz Matriz;
+
+struct elemento {
+    int linha;
+    int coluna;
+    int valor;
+
+    struct elemento *prox;
+    struct elemento *abaixo;
+}
+
+typedef struct elemento Elemento;
+
+//função para criar matriz
+//função vazia
+//funcao verificar existencia
+//funcao consultar nó
+//funcao insere final
+//função insere início
+//função tamanho matriz(x_y)
+//primeiraCelulaLinha()
